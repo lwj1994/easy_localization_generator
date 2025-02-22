7ee92429853a6d65df4b8a4a5c73068264674c82
wen <alwjlola@gmail.com>
2025-02-22 23:42:52 +0800

    upgrade dependency

--- example/lib/localization/strings.g.dart
+++ example/lib/localization/strings.g.dart
@@ -6,7 +6,7 @@ part of 'strings.dart';
 // LocalizationGenerator
 // **************************************************************************
 
-// Generated at: Thu, 28 Mar 2024 12:49:54 +08:00
+// Generated at: Sat, 22 Feb 2025 23:42:04 +08:00
 class Strings {
   static const supportedLocales = [
     Locale('en', 'US'),
